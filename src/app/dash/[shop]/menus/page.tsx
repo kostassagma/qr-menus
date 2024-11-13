68a7d75074ea4b2b235af710283ef50ee488a57f
Kostas <kostassagmatopoulos@gmail.com>
2024-11-13 17:00:56 +0200

    ficing new menu

--- src/app/dash/[shop]/menus/page.tsx
+++ src/app/dash/[shop]/menus/page.tsx
@@ -13,7 +13,7 @@ export default async function ShopPage({
     <ShopLayout tab={1} shop={shop}>
       <div className="flex-1">
         <h2 className="text-xl mb-3">Οι κατάλογοι του μαγαζιού:</h2>
-        <div className="grid grid-cols-4 gap-5 w-full">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full">
           {["a", "a", "a", "a", "a", "a", "a", "a", "a"].map((menu, i) => (
             <Link
               href={`/dash/${menu}`}
@@ -23,11 +23,15 @@ export default async function ShopPage({
               {menu}
             </Link>
           ))}
-          <Link href={`/dash/${shop}/new-menu`}>
-            <div className="rounded-lg p-5 border border-gray-300 hover:cursor-pointer hover:scale-105 transition ease-in-out hover:border-accent group">
-              <h1>Φτιάξε ένα νέο κατάλογο</h1>
-              <PlusIcon className="text-black w-5 mx-auto group-hover:text-accent group-hover:rotate-90 transition ease-in-out" />
-            </div>
+          <Link
+            href={`/dash/${shop}/new-menu`}
+            className="rounded-lg p-5 border border-gray-300 hover:cursor-pointer hover:scale-105 transition ease-in-out hover:border-gray-500 flex flex-row gap-2 group"
+          >
+            <PlusIcon
+              width={20}
+              className="group-hover:rotate-180 transition ease-in-out duration-300"
+            />
+            <h1 className="font-medium">Νέος κατάλογος</h1>
           </Link>
         </div>
       </div>
