682dbb57a67b884e262649c28bc452ba4deb0ace
Kostas <kostassagmatopoulos@gmail.com>
2024-11-28 16:31:55 +0200

    stop logging hostname

--- src/app/auth/google-oauth-button.tsx
+++ src/app/auth/google-oauth-button.tsx
@@ -6,8 +6,6 @@ import { createBrowserClient } from "@/utils/supabase/client";
 import { FC } from "react";
 
 const GoogleOAuthButton: FC = () => {
-  console.log(HOSTNAME);
-  
   return (
     <button
       onClick={async () => {
