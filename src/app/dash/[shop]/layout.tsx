4d71a6676cfcd83baa984587143438cb98e785b0
Kostas <kostassagmatopoulos@gmail.com>
2024-11-28 20:36:54 +0200

    side nav on side

--- src/app/dash/[shop]/layout.tsx
+++ src/app/dash/[shop]/layout.tsx
@@ -16,11 +16,13 @@ export default async function Layout({
     <>
       <div className="min-h-screen flex flex-col">
         <DashNav shop={shop} />
-        <main className="max-w-7xl mx-auto w-full flex flex-col px-0 sm:px-2 gap-2 sm:gap-5 sm:flex-row py-2">
+        <main className="w-full flex flex-col px-0 sm:px-2 gap-2 sm:gap-5 sm:flex-row py-2">
           <Suspense>
             <SideNav shopPathname={shop} />
           </Suspense>
-          <section className="flex-1 px-4 sm:px-0">{children}</section>
+          <section className="flex-1 px-4 sm:px-0 max-w-7xl w-full mx-auto">
+            {children}
+          </section>
         </main>
       </div>
       <BigFooter />
