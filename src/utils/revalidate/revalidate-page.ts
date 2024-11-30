908cfe37e6ce39780450368f67e98409cee4534e
Kostas <kostassagmatopoulos@gmail.com>
2024-11-30 16:06:18 +0200

    revalidating /en

--- src/utils/revalidate/revalidate-page.ts
+++ src/utils/revalidate/revalidate-page.ts
@@ -4,10 +4,10 @@ import { revalidatePath } from "next/cache";
 
 export async function revalidateMenuPage(pahtname: string) {
   revalidatePath(`/m/${pahtname}`, "page");
-  revalidatePath(`/m/${pahtname}/[lang]`, "page");
+  revalidatePath(`/m/${pahtname}/en`, "page");
 }
 
 export async function revalidateShopPage(pahtname: string) {
   revalidatePath(`/s/${pahtname}`, "page");
-  revalidatePath(`/s/${pahtname}/[lang]`, "page");
+  revalidatePath(`/s/${pahtname}/en`, "page");
 }
